31741800c1ef4071f9c027965ca5fd600bfea188
Wiliamfm <santifon15@gmail.com>
2021-11-29 20:32:27 -0500

    validate user

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
-var localurl= "http://192.168.0.107:8083/users";
+var localurl= "http://localhost:8083/users";
 var weburl= "https://wiliamfm.github.io/bd1-final-view";
 document.addEventListener('DOMContentLoaded', () => {
+  window.sessionStorage.removeItem("type");
   document.querySelector('#formLogin').addEventListener('submit', submitFormLogin);
 });
 
@@ -18,24 +19,25 @@ function submitFormLogin(event){
     body: JSON.stringify(data)
   }).then(response => {
     console.log(response.status);
-    console.log("RESPONSE", response.headers);
+    //console.log("RESPONSE", response.headers);
     let aux= false;
     if(response.status === 404){
       let div= document.querySelector('#divAlert');
       div.className= "alert alert-warning";
       div.textContent= "Usuario o contraseña son incorrectas";
       div.focus();
+      window.sessionStorage.removeItem("type");
     }else if(response.status === 302){
       console.log("user");
       window.sessionStorage.setItem("type", "user");
       aux= true;
-    }else if(response.status === 308){
-      console.log("admins");
+    }else if(response.status === 200){
+      console.log("admin");
       window.sessionStorage.setItem("type", "admin");
       aux= true;
     }
     if(aux){
-      window.location.replace(weburl+"/vetUser/index.html");
+      //window.location.replace(weburl+"/vetUser/index.html");
     }
   }).catch(error => {
     console.log("ERROR Login user: ", error);
